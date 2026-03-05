037eea0a8c24d8671e5c839cc636d2dcdc3e32b0
ericthecurious <hello@ericthecurious.com>
2026-03-04 23:58:21 -0800

    patch: remove comment

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -60,8 +60,6 @@ export default [
                 },
             ],
             '@typescript-eslint/no-empty-interface': 0,
-            // TODO: Remove this if we can; it isn't a good rule to squash.
-            // Sometimes this is fine, but sometimes it masks a compile error.
             '@typescript-eslint/ban-ts-ignore': 0,
             '@typescript-eslint/no-empty-function': 0,
             '@typescript-eslint/explicit-function-return-type': 0,
